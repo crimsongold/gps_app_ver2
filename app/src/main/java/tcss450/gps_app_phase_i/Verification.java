36c9ba3a1a1572d0e3ef545a118acd7be8e6bf5d
calebzj <calebzj@gmail.com>
2015-05-04 10:16:21 -0700

    Verification: Added the verification class

---
+++ app/src/main/java/tcss450/gps_app_phase_i/Verification.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2015. This product is a brain-product of Jacob Langholz, Jonathan Coons, and Caleb Jaeger. The collective content within was created by them and them alone to fulfill the requirements of the mobile gps application project for TCSS 450.
+ */
+
+package tcss450.gps_app_phase_i;
+
+/**
+ * Created by caleb on 5/4/15.
+ */
+public class Verification {
+
+    //TODO I think that these should return a string or result number. A boolean doesn't provide enough info.
+    public static boolean isEmailValid(final String email)
+    {
+        //pattern for valid password
+        String pattern = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+        //TODO: Replace this with your own logic
+        boolean result = email.matches(pattern);
+        return email.contains("@") ;//&& user_base.user_exist(email);
+    }
+
+    /**
+     * Checks to see whether or not the password is valid.
+     *
+     * @param password is the string representing the password to be checked.
+     * @return boolean representing whether or not the password is valid.
+     */
+    public static boolean isPasswordValid(final String password)
+    {
+        //TODO: Replace this with your own logic
+        return password.length() >= 7;
+    }
+}
