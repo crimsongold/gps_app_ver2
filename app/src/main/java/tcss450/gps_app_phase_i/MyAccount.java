7d33c5a1fc70b8f56b3ef04e554e7ff8497dff30
jonathancoons <joncoons@gmail.com>
2015-05-11 02:37:57 -0700

    added start and end buttos that bring up a date picker.  if no dates picked it will give last 7 days of data.  if only start or end is picked it will show a days worth of data.

--- app/src/main/java/tcss450/gps_app_phase_i/MyAccount.java
+++ app/src/main/java/tcss450/gps_app_phase_i/MyAccount.java
@@ -7,24 +7,27 @@
 package tcss450.gps_app_phase_i;
 
 import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.PendingIntent;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 
 /**
@@ -33,9 +36,14 @@ import java.util.Date;
  * it allows the user to reset the password
  */
 public class MyAccount extends ActionBarActivity {
+
     private LocalMapData location_data;
     private SharedPreferences prefs;
     private SharedPreferences.Editor prefs_editor;
+    //static Date tempDate;
+    static Date start;
+    static Date end;
+    static String dateFlag;
 
 
     @Override
@@ -51,58 +59,150 @@ public class MyAccount extends ActionBarActivity {
         prefs_editor = prefs.edit();
         startAlarm();
 
-        Date start;
+        //Date start;
+        //Date end;
 
-        EditText enter_start_date = (EditText) findViewById(R.id.enter_start_date);
-        /*startBox.setOnEditorActionListener(new TextView.OnEditorActionListener()
-        {
-
+        Button startDate = (Button) findViewById(R.id.start_button);
+        startDate.setOnClickListener(new OnClickListener() {
             @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
-            {
-                return false;
+            public void onClick(View v) {
+                showDatePickerDialog(v);
+                dateFlag = "start";
+                //start = new Date(tempDate.getDate());
+                //start = tempDate;
             }
-        });*/
 
-        EditText enter_start_time = (EditText) findViewById(R.id.enter_start_time);
-        /*startBox.setOnEditorActionListener(new TextView.OnEditorActionListener()
-        {
+        });
 
+
+        Button endDate = (Button) findViewById(R.id.end_button);
+        endDate.setOnClickListener(new OnClickListener() {
             @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
-            {
-                return false;
+            public void onClick(View v) {
+                showDatePickerDialog(v);
+                dateFlag = "end";
+                //end = new Date(tempDate.getDate());
+                //end = tempDate;
             }
-        });*/
+        });
 
-        EditText enter_end_date = (EditText) findViewById(R.id.enter_end_date);
-        /*startBox.setOnEditorActionListener(new TextView.OnEditorActionListener()
-        {
 
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
-            {
-                return false;
-            }
-        });*/
 
-        EditText enter_end_time = (EditText) findViewById(R.id.enter_end_time);
-        /*startBox.setOnEditorActionListener(new TextView.OnEditorActionListener()
-        {
 
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
-            {
-                return false;
-            }
-        });*/
+
 
         Button viewData = (Button) findViewById(R.id.view_data_button);
         viewData.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MyAccount.this, MovementData.class);
-                startActivity(intent);
+                //set shared preferences date
+
+                if(start == null || end == null){
+
+                    if(start == null && end == null) {
+                        Calendar cal = GregorianCalendar.getInstance();
+                        cal.setTime(new Date());
+                        cal.add(Calendar.DAY_OF_YEAR, -7);
+                        start = cal.getTime();
+
+                        end = new Date(Calendar.DATE);
+
+                        Context context = getApplicationContext();
+                        CharSequence text = "Last 7 days shown";
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+
+                        Intent intent = new Intent(MyAccount.this, MovementData.class);
+                        startActivity(intent);
+
+                        prefs_editor.putLong("startTime", start.getTime());
+                        prefs_editor.putLong("endTime", end.getTime());
+                        prefs_editor.commit();
+
+
+                    }
+
+                    if(start == null && end != null) {
+                        Calendar cal = GregorianCalendar.getInstance();
+                        cal.setTime(end);
+                        cal.set(Calendar.DATE, -1);
+                        start = cal.getTime();
+
+                        Context context = getApplicationContext();
+                        CharSequence text = "Showing 1 day before end date";
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+
+                        Intent intent = new Intent(MyAccount.this, MovementData.class);
+                        startActivity(intent);
+
+                        prefs_editor.putLong("startTime", start.getTime());
+                        prefs_editor.putLong("endTime", end.getTime());
+                        prefs_editor.commit();
+
+
+                        //cal.add(end.getDay(), -1);
+
+                        //start = new Date(end.getDate());
+
+//                        Calendar cal = GregorianCalendar.getInstance();
+//                        cal.setTime(new Date());
+//                        cal.add(Calendar.DAY_OF_YEAR, -1);
+//                        start = cal.getTime();
+                    }
+
+
+
+                    if(end == null && start != null) {
+
+
+
+                        Calendar cal = GregorianCalendar.getInstance();
+                        cal.setTime(start);
+                        cal.set(Calendar.DATE, 1);
+                        end = cal.getTime();
+
+                        Context context = getApplicationContext();
+                        CharSequence text = "Showing 1 day past start date";
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+
+                        Intent intent = new Intent(MyAccount.this, MovementData.class);
+                        startActivity(intent);
+
+                        prefs_editor.putLong("startTime", start.getTime());
+                        prefs_editor.putLong("endTime", end.getTime());
+                        prefs_editor.commit();
+                    }
+
+
+
+//                    Context context = getApplicationContext();
+//                    CharSequence text = "Last 7 days shown";
+//                    int duration = Toast.LENGTH_SHORT;
+//
+//                    Toast toast = Toast.makeText(context, text, duration);
+//                    toast.show();
+//
+//                    Intent intent = new Intent(MyAccount.this, MovementData.class);
+//                    startActivity(intent);
+
+
+                } else {
+
+                    prefs_editor.putLong("startTime", start.getTime());
+                    prefs_editor.putLong("endTime", end.getTime());
+                    prefs_editor.commit();
+                    Intent intent = new Intent(MyAccount.this, MovementData.class);
+                    startActivity(intent);
+                }
+
             }
         });
 
@@ -174,4 +274,50 @@ public class MyAccount extends ActionBarActivity {
         AlarmManager alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
         alarm.cancel(pIntent);
     }
+
+
+
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            if(dateFlag.equals("start"))
+            {
+                start = new Date(year, month, day);
+            } else if (dateFlag.equals("end")){
+                end = new Date(year, month, day);
+            }
+            // Do something with the date chosen by the user
+        }
+    }
+
+
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "Start Date");
+
+    }
+
+
+    public void showDatePickerDialogEnd(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "End Date");
+
+    }
+
+
 }
